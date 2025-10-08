d8efc3ce9a40b6df439a0fd0fcfab118278f9cb7
terryye <i@terryye.com>
2025-10-08 16:45:10 -0700

    Week5

---
+++ src/flash_attention_v2/02_cuda_attention2_parallelism/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd ../.. # go to the root directory
+modal run scripts/modal_nvcc.py --code-path week_05/00_cuda_attention2_forward/main.cu
---
+++ src/flash_attention_v2/readme.md
@@ -0,0 +1,10 @@
+# Week5 Assignment
+
+Implement flash attention2 in CUDA.
+
+test method:
+enter each folder
+
+```bash
+./run.sh
+```
