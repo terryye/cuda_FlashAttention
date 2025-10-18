9abfbcc5485fdb4d942617df975af0e93d6da11d
terryye <i@terryye.com>
2025-10-17 17:21:40 -0700

    change shell dir

--- src/flash_attention_v2/02_cuda_attention2_forward_parall/run.sh
+++ src/flash_attention_v2/02_cuda_attention2_forward_parall/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd ../.. # go to the root directory
-modal run scripts/modal_nvcc.py --code-path week_05/02_cuda_attention2_forward_warps/main.cu
+modal run scripts/modal_nvcc.py --code-path week_05/02_cuda_attention2_forward_parall/main.cu
