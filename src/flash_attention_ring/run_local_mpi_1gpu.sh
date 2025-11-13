2d78e2b291eeae4e900a74c4cfca45602b87fcd3
terryye <i@terryye.com>
2025-11-13 15:40:51 -0800

    refactor deepseek moe

---
+++ src/flash_attention_ring/run_local_mpi_1gpu.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Get the directory containing this script
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+# Calculate relative path from repo root to script directory
+RELATIVE_PATH=${SCRIPT_DIR#$(pwd)/}
+
+../../scripts/run_local_mpi_1gpu.sh $RELATIVE_PATH/$1
