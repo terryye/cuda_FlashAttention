223a02ff9a39b8ce06e2f0a8a67efe9046797a8a
terryye <i@terryye.com>
2025-12-17 07:09:31 +0000

    improve some configuration file

--- src/flash_attention_ring/run_local_gpu.sh
+++ src/flash_attention_ring/run_local_gpu.sh
@@ -5,4 +5,4 @@ SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 # Calculate relative path from repo root to script directory
 RELATIVE_PATH=${SCRIPT_DIR#$(pwd)/}
 
-../../scripts/run_local_mpi_1gpu.sh $RELATIVE_PATH/$1
+../../scripts/run_local_gpu.sh $RELATIVE_PATH/$1
